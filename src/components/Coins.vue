a289d3e9be58089361595c29d728c84db02e273f
Attilius <leo20@freemail.hu>
2022-09-04 22:32:48 +0200

    Added Coins.vue

---
+++ src/components/Coins.vue
@@ -0,0 +1,49 @@
+<template>
+  <div class="coins">
+    <div id="coin"></div>
+    <div id="available-coins">382</div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Coins",
+};
+</script>
+
+<style>
+.coins {
+    display: flex;
+    justify-content: space-around;
+    align-items: center;
+    width: 20%;
+    height: 60%;
+}
+
+#available-coins{
+    height: 100%;
+    width: 100%;
+    border-radius: 50px;
+    background-color: rgb(73, 62, 84);
+    border: 1px solid goldenrod;
+    font-family: 'Emilys Candy', cursive;
+    font-size: 22px;
+    text-align: center;
+    text-shadow: goldenrod 1px 1px;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    color: #FFF8DC;
+}
+
+#coin {
+    height: 140%;
+    width: 40%;
+    background: url("../assets/images/coin.png") no-repeat center;
+    background-size: cover;
+    position: relative;
+    bottom: 0;
+    left: 30px;
+}
+
+</style>
