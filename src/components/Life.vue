0fcce999016613de0c0857b52ae2e4035496152c
Attilius <leo20@freemail.hu>
2022-09-04 22:34:23 +0200

    Added Life.vue

---
+++ src/components/Life.vue
@@ -0,0 +1,58 @@
+<template>
+  <div class="life">
+    <div id="heart">{{ life }}</div>
+    <div id="life-regeneration">{{ life_status }}</div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Life",
+  props: ['life', 'life_status']
+};
+</script>
+
+<style>
+.life {
+    display: flex;
+    justify-content: space-around;
+    align-items: center;
+    width: 20%;
+    height: 60%;
+}
+
+#life-regeneration{
+    height: 100%;
+    width: 100%;
+    border-radius: 50px;
+    padding-left: 20px;
+    background-color: rgb(73, 62, 84);
+    border: 1px solid goldenrod;
+    font-family: 'Emilys Candy', cursive;
+    font-size: 22px;
+    text-shadow: goldenrod 1px 1px;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    color: #FFF8DC;
+}
+
+#heart {
+    height: 140%;
+    width: 55%;
+    background: url("../assets/images/heart.png") no-repeat;
+    background-size: cover;
+    font-family: 'Emilys Candy', cursive;
+    font-size: 20px;
+    text-align: center;
+    text-shadow: goldenrod 1px 1px;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    color: #FFF8DC;
+    position: relative;
+    bottom: 2px;
+    left: 35px;
+}
+
+</style>
