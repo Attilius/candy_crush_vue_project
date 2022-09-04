9bb35edb5cd7edcc65e7c2ba55b2ecb9bcbdf0b1
Attilius <leo20@freemail.hu>
2022-09-04 22:34:09 +0200

    Added Header.vue

---
+++ src/components/Header.vue
@@ -0,0 +1,41 @@
+<template>
+    <div class="header">
+        <Life :life="life" :life_status="life_status" />
+        <Star />
+        <Coins />
+    </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import Life from "@/components/Life.vue";
+import Star from "@/components/Stars.vue";
+import Coins from "@/components/Coins.vue";
+
+export default {
+    name: "Header",
+    props: ['life', 'life_status'],
+    components: {
+        Life,
+        Star,
+        Coins
+    }
+};
+</script>
+
+<style>
+.header {
+    width: 800px;
+    height: 60px;
+    background: url("../assets/images/header_background.jpg") no-repeat center fixed;
+    z-index: 11;
+    position: fixed;
+    top: 35px;
+    left: 25%;
+    border-bottom-left-radius: 30px;
+    border-bottom-right-radius: 30px;
+    display: flex;
+    justify-content: space-around;
+    align-items: center;
+}
+</style>
