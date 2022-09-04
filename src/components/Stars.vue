6fae0425eafc0e7898a5fda5ec3b7d9b66e6b371
Attilius <leo20@freemail.hu>
2022-09-04 22:34:55 +0200

    Added Stars.vue

---
+++ src/components/Stars.vue
@@ -0,0 +1,49 @@
+<template>
+  <div class="stars">
+    <div id="star"></div>
+    <div id="earned-stars">162</div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Stars",
+};
+</script>
+
+<style>
+.stars {
+    display: flex;
+    justify-content: space-around;
+    align-items: center;
+    width: 20%;
+    height: 60%;
+}
+
+#earned-stars {
+    height: 100%;
+    width: 100%;
+    border-radius: 50px;
+    background-color: rgb(73, 62, 84);
+    border: 1px solid goldenrod;
+    font-family: 'Emilys Candy', cursive;
+    font-size: 22px;
+    text-align: center;
+    text-shadow: goldenrod 1px 1px;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    color: #FFF8DC;
+}
+
+#star {
+    height: 120%;
+    width: 40%;
+    background: url("../assets/images/stars.png") no-repeat center;
+    background-size: cover;
+    position: relative;
+    bottom: 0;
+    left: 30px;
+}
+
+</style>
