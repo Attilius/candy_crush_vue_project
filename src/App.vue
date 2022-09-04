e4d1f9b201a357a8edcec7d53db9c56f9bcc90be
Attilius <leo20@freemail.hu>
2022-09-04 22:32:19 +0200

    Added App.vue

---
+++ src/App.vue
@@ -0,0 +1,13 @@
+<template>
+  <div id="app">
+      <router-link to="/"></router-link>
+    <router-view />
+  </div>
+</template>
+
+<style>
+@import url('https://fonts.googleapis.com/css2?family=Emilys+Candy&family=Open+Sans:wght@400;500&family=Roboto+Mono:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;1,100;1,200;1,300;1,400;1,500;1,600;1,700&display=swap');
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+}
+</style>
