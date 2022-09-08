49d8cd86b498f950e43a85c2fa40fef3b1b2dcba
Attilius <leo20@freemail.hu>
2022-09-08 19:12:09 +0200

    Added SoundButton.vue

---
+++ src/components/SoundButton.vue
@@ -0,0 +1,23 @@
+<template>
+  <div>
+    <div class="sound" id="sound" @click="soundToggle">
+      <i class="fa-solid fa-volume-high"></i>
+    </div>
+    <audio
+      ref="audio"
+      src="@/assets/sounds/Longloop.mp3"
+      preload
+      loop
+      id="audio"
+      autoplay
+    ></audio>
+  </div>
+</template>
+
+<script>
+export default {
+    name: "SoundButton"
+};
+</script>
+
+<style></style>
