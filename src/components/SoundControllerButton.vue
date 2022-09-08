6084b1420e31ddcc45f5fbc06598215ce692a1fe
Attilius <leo20@freemail.hu>
2022-09-08 19:05:10 +0200

    Modified SoundControllerButton.vue

--- src/components/SoundControllerButton.vue
+++ src/components/SoundControllerButton.vue
@@ -1,5 +1,17 @@
 <template>
-  
+  <div>
+      <div class="sound" id="sound" @click="soundToggle">
+        <i class="fa-solid fa-volume-high"></i>
+      </div>
+      <audio
+      ref="audio"
+      src="@/assets/sounds/Longloop.mp3"
+      preload
+      loop
+      id="audio"
+      autoplay
+    ></audio>
+    </div>
 </template>
 
 <script>
