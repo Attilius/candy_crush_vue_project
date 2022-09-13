3219bb9319fe0686e1cb214e901f0e4ca8ea23ad
Attilius <leo20@freemail.hu>
2022-09-13 22:11:37 +0200

    Modified MainPageView.vue

--- src/views/MainPageView.vue
+++ src/views/MainPageView.vue
@@ -132,7 +132,7 @@ export default {
 .map {
   width: 700px;
   height: 93vh;
-  background: url("../assets/images/map.png") no-repeat center scr;
+  background: url("../assets/images/map.png") no-repeat center sc;
   background-size: cover;
   position: absolute;
   top: 52px;
