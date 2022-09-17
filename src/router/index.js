63a42fbac821e317e024b09444d57520f0463fe4
Attilius <leo20@freemail.hu>
2022-09-17 19:44:52 +0200

    Modified index.js (router)

--- src/router/index.js
+++ src/router/index.js
@@ -109,6 +109,15 @@ const routes = [
     component: () =>
       import(/* webpackChunkName: "about" */ "../views/AboutView.vue"),
   },
+  {
+    path: "/level_12",
+    name: "level_12",
+    // route level code-splitting
+    // this generates a separate chunk (about.[hash].js) for this route
+    // which is lazy-loaded when the route is visited.
+    component: () =>
+      import(/* webpackChunkName: "about" */ "../views/AboutView.vue"),
+  },
 ];
 
 const router = new VueRouter({
