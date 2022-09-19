61ece57aea7ea2fa2e55c22dc2cb28aef6f2ce5d
Attilius <leo20@freemail.hu>
2022-09-19 10:09:06 +0200

    Modified GameBoard.vue

--- src/views/GameBoard.vue
+++ src/views/GameBoard.vue
@@ -1,13 +1,15 @@
 <template>
-  
+  <div class="game-board"></div>
 </template>
 
 <script>
-export default {
-
-}
+export default {};
 </script>
 
 <style>
-
+.game-board {
+  height: 99vh;
+  width: 100%;
+  background: url("../assets/images/background.jpg") no-repeat left;
+}
 </style>
