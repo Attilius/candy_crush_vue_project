2c717f2ce7676de3c906ba3ce869dd670ff87835
Attilius <leo20@freemail.hu>
2022-09-19 11:46:53 +0200

    Modified ScoreBoard.vue

--- src/components/ScoreBoard.vue
+++ src/components/ScoreBoard.vue
@@ -1,15 +1,39 @@
 <template>
-  <div>
+  <div class="score-board">
+    <div class="moves">
 
+    </div>
   </div>
 </template>
 
 <script>
 export default {
-    name: "ScoreBoard"
-}
+  name: "ScoreBoard",
+};
 </script>
 
 <style>
+.score-board {
+  background-color: cornflowerblue;
+  color: white;
+  width: 250px;
+  height: 565px;
+  position: absolute;
+  left: 250px;
+  top: 120px;
+  border-radius: 20px;
+  display: flex;
+  justify-content: space-between;
+  flex-direction: column;
+}
 
+.moves {
+  width: 120px;
+  height: 120px;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  padding-top: 6px;
+}
 </style>
