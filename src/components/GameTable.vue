ebc44a7ef7eacc2aece6a26aa21bdc74fae36f9d
Attilius <leo20@freemail.hu>
2022-09-21 18:51:45 +0200

    Modified GameTable.vue

--- src/components/GameTable.vue
+++ src/components/GameTable.vue
@@ -1,6 +1,21 @@
 <template>
   <div id="game-table">
-    
+    <img v-for="candy in candies" :key="candy.id"
+        :src="'/img/' + candy.candy_image"
+        :alt="candy.alt"
+        :id="candy.id"
+        :style="{
+            background : candy.candy_image === '' ? 'transparent' : '#465a7abf',
+            visibility : candy.candy_image === '' ? 'hidden' : 'visible'
+            }"
+        :draggable="true"
+        @dragover="(e) => e.preventDefault()"
+        @dragenter="(e) => e.preventDefault()"
+        @dragleave="(e) => e.preventDefault()"
+        @dragstart="dragStart"
+        @drop="dragDrop"
+        @dragend="dragEnd"
+    />
   </div>
 </template>
 
