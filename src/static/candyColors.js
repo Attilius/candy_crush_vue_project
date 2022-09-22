1869fa4265d49f8ae888ac284e8d41dabe0f243d
Attilius <leo20@freemail.hu>
2022-09-22 16:07:32 +0200

    Added candyColors.js

---
+++ src/static/candyColors.js
@@ -0,0 +1,28 @@
+const candyColors = [
+    {
+        src: "blue.png", 
+        alt: 'blue'
+    },
+    {
+        src: "green.png", 
+        alt: 'green'
+    },
+    {
+        src: "orange.png", 
+        alt: 'orange'
+    },
+    {
+        src: "purple.png", 
+        alt: 'purple'
+    },
+    {
+        src: "red.png", 
+        alt: 'red'
+    },
+    {
+        src: "yellow.png", 
+        alt: 'yellow'
+    }
+];
+
+export default candyColors;
