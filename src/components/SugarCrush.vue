6f0fb34ac7d8b933afb4255ebddb50aa31acc44e
Attilius <leo20@freemail.hu>
2022-09-23 17:35:24 +0200

    Added SugarCrush.vue

---
+++ src/components/SugarCrush.vue
@@ -0,0 +1,40 @@
+<template>
+  <div class="sugar-crush" :style="{visibility: visibility}">
+    <h1>SUGAR CRUSH!</h1>
+    <audio v-if="visibility == 'visible'"
+      ref="audio"
+      src="@/assets/sounds/Sugar_Crush.mp3"
+      preload
+      autoplay
+    ></audio>
+  </div>
+</template>
+
+<script>
+export default {
+    name: 'SugarCrush',
+    props: ['visibility']
+
+}
+</script>
+
+<style>
+.sugar-crush {
+  width: 506px;
+  height: 505px;
+  background: transparent;
+  position: absolute;
+  left: 520px;
+  top: 150px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+
+.sugar-crush h1 {
+  color: #fff8dc;
+  text-shadow: -3px 0 red, 0 3px red, 3px 0 red, 0 -3px red;
+  font-family: "Emilys Candy", cursive;
+  font-size: 67px;
+}
+</style>
