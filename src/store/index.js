b772dd51e549bc8bc67a42efcc507532061e9762
Attilius <leo20@freemail.hu>
2022-09-25 20:30:43 +0200

    Modified index.js (src/store)

--- src/store/index.js
+++ src/store/index.js
@@ -1,260 +1,279 @@
 import Vue from "vue";
 import Vuex from "vuex";
+import createPersistedState from "vuex-persistedstate";
 
 Vue.use(Vuex);
 
 export default new Vuex.Store({
-  state: {
-    candies: [
-      {
-          id : 1,
-          image : "red.png",
-          alt : "red"
-      },
-      {
-          id : 2,
-          image : "purple.png",
-          alt : "purple"
-      },
-      {
-          id : 3,
-          image : "yellow.png",
-          alt : "yellow"
-      },
-      {
-          id : 4,
-          image : "blue.png",
-          alt : "blue"
-      },
-      {
-          id : 5,
-          image : "yellow.png",
-          alt : "yellow"
-      },
-      {
-          id : 6,
-          image : "yellow.png",
-          alt : "yellow"
-      },
-      {
-          id : 7,
-          image : "red.png",
-          alt : "red"
-      },
-      {
-          id : 8,
-          image : "",
-          alt : ""
-      },
-      {
-          id : 9,
-          image : "purple.png",
-          alt : "purple"
-      },
-      {
-          id : 10,
-          image : "yellow.png",
-          alt : "yellow"
-      },
-      {
-          id : 11,
-          image : "green.png",
-          alt : "green"
-      },
-      {
-          id : 12,
-          image : "blue.png",
-          alt : "blue"
-      },
-      {
-          id : 13,
-          image : "blue.png",
-          alt : "blue"
-      },
-      {
-          id : 14,
-          image : "",
-          alt : ""
-      },
-      {
-          id : 15,
-          image : "green.png",
-          alt : "green"
-      },
-      {
-          id : 16,
-          image : "green.png",
-          alt : "green"
-      },
-      {
-          id : 17,
-          image : "blue.png",
-          alt : "blue"
-      },
-      {
-          id : 18,
-          image : "red.png",
-          alt : "red"
-      },
-      {
-          id : 19,
-          image : "yellow.png",
-          alt : "yellow"
-      },
-      {
-          id : 20,
-          image : "purple.png",
-          alt : "purple"
-      },
-      {
-          id : 21,
-          image : "yellow.png",
-          alt : "yellow"
-      },
-      {
-          id : 22,
-          image : "green.png",
-          alt : "green"
-      },
-      {
-          id : 23,
-          image : "yellow.png",
-          alt : "yellow"
-      },
-      {
-          id : 24,
-          image : "red.png",
-          alt : "red"
-      },
-      {
-          id : 25,
-          image : "blue.png",
-          alt : "blue"
-      },
-      {
-          id : 26,
-          image : "red.png",
-          alt : "red"
-      },
-      {
-          id : 27,
-          image : "blue.png",
-          alt : "blue"
-      },
-      {
-          id : 28,
-          image : "purple.png",
-          alt : "purple"
-      },
-      {
-          id : 29,
-          image : "yellow.png",
-          alt : "yellow"
-      },
-      {
-          id : 30,
-          image : "yellow.png",
-          alt : "yellow"
-      },
-      {
-          id : 31,
-          image : "purple.png",
-          alt : "purple"
-      },
-      {
-          id : 32,
-          image : "green.png",
-          alt : "green"
-      },
-      {
-          id : 33,
-          image : "purple.png",
-          alt : "purple"
-      },
-      {
-          id : 34,
-          image : "yellow.png",
-          alt : "yellow"
-      },
-      {
-          id : 35,
-          image : "green.png",
-          alt : "green"
-      },
-      {
-          id : 36,
-          image : "",
-          alt : ""
-      },
-      {
-          id : 37,
-          image : "green.png",
-          alt : "green"
-      },
-      {
-          id : 38,
-          image : "green.png",
-          alt : "green"
-      },
-      {
-          id : 39,
-          image : "yellow.png",
-          alt : "yellow"
-      },
-      {
-          id : 40,
-          image : "yellow.png",
-          alt : "yellow"
-      },
-      {
-          id : 41,
-          image : "green.png",
-          alt : "green"
-      },
-      {
-          id : 42,
-          image : "",
-          alt : ""
-      },
-      {
-          id : 43,
-          image : "red.png",
-          alt : "red"
-      },
-      {
-          id : 44,
-          image : "blue.png",
-          alt : "blue"
-      },
-      {
-          id : 45,
-          image : "purple.png",
-          alt : "purple"
-      },
-      {
-          id : 46,
-          image : "green.png",
-          alt : "green"
-      },
-      {
-          id : 47,
-          image : "purple.png",
-          alt : "purple"
-      },
-      {
-          id : 48,
-          image : "yellow.png",
-          alt : "yellow"
-      },
-      {
-          id : 49,
-          image : "red.png",
-          alt : "red"
-      },
-  ]
-  },
-  getters: {},
-  mutations: {},
-  actions: {},
-  modules: {},
+    state: {
+        candies: [
+            {
+                id: 1,
+                image: "red.png",
+                alt: "red"
+            },
+            {
+                id: 2,
+                image: "purple.png",
+                alt: "purple"
+            },
+            {
+                id: 3,
+                image: "yellow.png",
+                alt: "yellow"
+            },
+            {
+                id: 4,
+                image: "blue.png",
+                alt: "blue"
+            },
+            {
+                id: 5,
+                image: "yellow.png",
+                alt: "yellow"
+            },
+            {
+                id: 6,
+                image: "yellow.png",
+                alt: "yellow"
+            },
+            {
+                id: 7,
+                image: "red.png",
+                alt: "red"
+            },
+            {
+                id: 8,
+                image: "",
+                alt: ""
+            },
+            {
+                id: 9,
+                image: "purple.png",
+                alt: "purple"
+            },
+            {
+                id: 10,
+                image: "yellow.png",
+                alt: "yellow"
+            },
+            {
+                id: 11,
+                image: "green.png",
+                alt: "green"
+            },
+            {
+                id: 12,
+                image: "blue.png",
+                alt: "blue"
+            },
+            {
+                id: 13,
+                image: "blue.png",
+                alt: "blue"
+            },
+            {
+                id: 14,
+                image: "",
+                alt: ""
+            },
+            {
+                id: 15,
+                image: "green.png",
+                alt: "green"
+            },
+            {
+                id: 16,
+                image: "green.png",
+                alt: "green"
+            },
+            {
+                id: 17,
+                image: "blue.png",
+                alt: "blue"
+            },
+            {
+                id: 18,
+                image: "red.png",
+                alt: "red"
+            },
+            {
+                id: 19,
+                image: "yellow.png",
+                alt: "yellow"
+            },
+            {
+                id: 20,
+                image: "purple.png",
+                alt: "purple"
+            },
+            {
+                id: 21,
+                image: "yellow.png",
+                alt: "yellow"
+            },
+            {
+                id: 22,
+                image: "green.png",
+                alt: "green"
+            },
+            {
+                id: 23,
+                image: "yellow.png",
+                alt: "yellow"
+            },
+            {
+                id: 24,
+                image: "red.png",
+                alt: "red"
+            },
+            {
+                id: 25,
+                image: "blue.png",
+                alt: "blue"
+            },
+            {
+                id: 26,
+                image: "red.png",
+                alt: "red"
+            },
+            {
+                id: 27,
+                image: "blue.png",
+                alt: "blue"
+            },
+            {
+                id: 28,
+                image: "purple.png",
+                alt: "purple"
+            },
+            {
+                id: 29,
+                image: "yellow.png",
+                alt: "yellow"
+            },
+            {
+                id: 30,
+                image: "yellow.png",
+                alt: "yellow"
+            },
+            {
+                id: 31,
+                image: "purple.png",
+                alt: "purple"
+            },
+            {
+                id: 32,
+                image: "green.png",
+                alt: "green"
+            },
+            {
+                id: 33,
+                image: "purple.png",
+                alt: "purple"
+            },
+            {
+                id: 34,
+                image: "yellow.png",
+                alt: "yellow"
+            },
+            {
+                id: 35,
+                image: "green.png",
+                alt: "green"
+            },
+            {
+                id: 36,
+                image: "",
+                alt: ""
+            },
+            {
+                id: 37,
+                image: "green.png",
+                alt: "green"
+            },
+            {
+                id: 38,
+                image: "green.png",
+                alt: "green"
+            },
+            {
+                id: 39,
+                image: "yellow.png",
+                alt: "yellow"
+            },
+            {
+                id: 40,
+                image: "yellow.png",
+                alt: "yellow"
+            },
+            {
+                id: 41,
+                image: "green.png",
+                alt: "green"
+            },
+            {
+                id: 42,
+                image: "",
+                alt: ""
+            },
+            {
+                id: 43,
+                image: "red.png",
+                alt: "red"
+            },
+            {
+                id: 44,
+                image: "blue.png",
+                alt: "blue"
+            },
+            {
+                id: 45,
+                image: "purple.png",
+                alt: "purple"
+            },
+            {
+                id: 46,
+                image: "green.png",
+                alt: "green"
+            },
+            {
+                id: 47,
+                image: "purple.png",
+                alt: "purple"
+            },
+            {
+                id: 48,
+                image: "yellow.png",
+                alt: "yellow"
+            },
+            {
+                id: 49,
+                image: "red.png",
+                alt: "red"
+            },
+        ],
+        life: 20
+    },
+    getters: {
+        life(state) {
+            return state.life;
+        }
+    },
+    mutations: {
+        decrement(state, step) {
+            state.life -= step;
+        },
+
+        increment(state, step) {
+            state.life += step;
+        }
+    },
+    actions: {},
+    modules: {},
+    plugins: [createPersistedState(
+        {
+            paths: ['life']
+        }
+    )],
 });
