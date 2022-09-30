85fa9f9ae9357f26ca52ec623d720b7a2d5b9a4e
Attilius <leo20@freemail.hu>
2022-09-30 20:08:16 +0200

    Modified index.js (src/router)

--- src/router/index.js
+++ src/router/index.js
@@ -37,9 +37,6 @@ const routes = [
   {
     path: "/level_5",
     name: "level_5",
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
     component: () =>
       import(/* webpackChunkName: "about" */ "../views/GameBoard.vue"),
   },
