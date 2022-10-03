5989213619a8dde9f478cadda9eca8b0b9e1c4d2
Attilius <leo20@freemail.hu>
2022-10-03 19:36:24 +0200

    Modified GameTable.vue

--- src/components/GameTable.vue
+++ src/components/GameTable.vue
@@ -219,7 +219,7 @@ export default {
     checkForDoubleThreeDownLeft(score = 60) {
       for (let i = 0; i < 48; i++) {
         const rowOfUpLeftDoubleThree = [i, i - 1, i - 2, i + 7, i + 7 * 2];
-        const decidedColor = this.$store.state.candies[i].image;
+        const decidedColor = this.$store.state.candies[i].alt;
         const notValid = [
           0, 1, 6, 7, 8, 9, 13, 14, 15, 21, 22, 27, 28, 29, 34, 
           35, 36, 37, 38, 39, 40, 41, 42, 43, 44, 45, 46, 47, 48,
