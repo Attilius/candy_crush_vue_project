ba966d829f6c4ac918f73b44dc4e853ba4c73a51
Attilius <leo20@freemail.hu>
2022-10-08 19:14:38 +0200

    Modified GameBoard.vue

--- src/views/GameBoard.vue
+++ src/views/GameBoard.vue
@@ -23,6 +23,7 @@
 </template>
 
 <script>
+// @ is an alias to /src
 import Navbar from "../components/Navbar.vue";
 import ScoreBoard from "../components/ScoreBoard.vue";
 import GameTable from "../components/GameTable.vue";
