25d862ccbc0d0902306539318db0659134733d60
Attilius <leo20@freemail.hu>
2022-10-11 20:10:09 +0200

    Modified bonusCandies.js

--- src/static/bonusCandies.js
+++ src/static/bonusCandies.js
@@ -2,6 +2,10 @@ const bonusCandies = [
     {
         src: "Blue-Striped-Horizontal.png",
         alt: 'blue'
+    },
+    {
+        src: "Blue-Striped-Verical.png",
+        alt: 'blue'
     }
 ];
 
