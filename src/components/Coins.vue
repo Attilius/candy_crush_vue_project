1175ce7ed67e494e967545025f9cc70e3bc4bb94
Attilius <leo20@freemail.hu>
2022-10-11 21:21:10 +0200

    Modified Coins.vue

--- src/components/Coins.vue
+++ src/components/Coins.vue
@@ -8,43 +8,43 @@
 <script>
 export default {
   name: "Coins",
-  props: ['coins']
+  props: ["coins"],
 };
 </script>
 
 <style>
 .coins {
-    display: flex;
-    justify-content: space-around;
-    align-items: center;
-    width: 20%;
-    height: 60%;
+  display: flex;
+  justify-content: space-around;
+  align-items: center;
+  width: 20%;
+  height: 60%;
 }
 
 #available-coins {
-    height: 100%;
-    width: 100%;
-    border-radius: 50px;
-    background-color: rgb(73, 62, 84);
-    border: 1px solid goldenrod;
-    font-family: 'Emilys Candy', cursive;
-    font-size: 22px;
-    text-align: center;
-    text-shadow: -1px 0 firebrick, 0 1px firebrick, 1px 0 firebrick, 0 -1px firebrick;
-    display: flex;
-    justify-content: center;
-    align-items: center;
-    color: #FFF8DC;
+  height: 100%;
+  width: 100%;
+  border-radius: 50px;
+  background-color: rgb(73, 62, 84);
+  border: 1px solid goldenrod;
+  font-family: "Emilys Candy", cursive;
+  font-size: 22px;
+  text-align: center;
+  text-shadow: -1px 0 firebrick, 0 1px firebrick, 1px 0 firebrick,
+    0 -1px firebrick;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  color: #fff8dc;
 }
 
 #coin {
-    height: 140%;
-    width: 40%;
-    background: url("../assets/images/coin.png") no-repeat center;
-    background-size: cover;
-    position: relative;
-    bottom: 0;
-    left: 30px;
+  height: 140%;
+  width: 40%;
+  background: url("../assets/images/coin.png") no-repeat center;
+  background-size: cover;
+  position: relative;
+  bottom: 0;
+  left: 30px;
 }
-
 </style>
