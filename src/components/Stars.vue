3f630f80bb34d63b387572934d283180789c3214
Attilius <leo20@freemail.hu>
2022-10-12 20:11:52 +0200

    Modified Stars.vue

--- src/components/Stars.vue
+++ src/components/Stars.vue
@@ -13,37 +13,37 @@ export default {
 
 <style>
 .stars {
-    display: flex;
-    justify-content: space-around;
-    align-items: center;
-    width: 20%;
-    height: 60%;
+  display: flex;
+  justify-content: space-around;
+  align-items: center;
+  width: 20%;
+  height: 60%;
 }
 
 #earned-stars {
-    height: 100%;
-    width: 100%;
-    border-radius: 50px;
-    background-color: rgb(73, 62, 84);
-    border: 1px solid goldenrod;
-    font-family: 'Emilys Candy', cursive;
-    font-size: 22px;
-    text-align: center;
-    text-shadow: -1px 0 firebrick, 0 1px firebrick, 1px 0 firebrick, 0 -1px firebrick;
-    display: flex;
-    justify-content: center;
-    align-items: center;
-    color: #FFF8DC;
+  height: 100%;
+  width: 100%;
+  border-radius: 50px;
+  background-color: rgb(73, 62, 84);
+  border: 1px solid goldenrod;
+  font-family: "Emilys Candy", cursive;
+  font-size: 22px;
+  text-align: center;
+  text-shadow: -1px 0 firebrick, 0 1px firebrick, 1px 0 firebrick,
+    0 -1px firebrick;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  color: #fff8dc;
 }
 
 #star {
-    height: 120%;
-    width: 40%;
-    background: url("../assets/images/star.png") no-repeat center;
-    background-size: cover;
-    position: relative;
-    bottom: 0;
-    left: 30px;
+  height: 120%;
+  width: 40%;
+  background: url("../assets/images/star.png") no-repeat center;
+  background-size: cover;
+  position: relative;
+  bottom: 0;
+  left: 30px;
 }
-
 </style>
