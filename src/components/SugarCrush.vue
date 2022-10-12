279bb868ed44100eb49b92c38820aa70afea2554
Attilius <leo20@freemail.hu>
2022-10-12 20:11:04 +0200

    Modified SugarCrush.vue

--- src/components/SugarCrush.vue
+++ src/components/SugarCrush.vue
@@ -1,7 +1,8 @@
 <template>
-  <div class="sugar-crush" :style="{visibility: visibility}">
+  <div class="sugar-crush" :style="{ visibility: visibility }">
     <h1>SUGAR CRUSH!</h1>
-    <audio v-if="visibility == 'visible'"
+    <audio
+      v-if="visibility == 'visible'"
       ref="audio"
       src="@/assets/sounds/Sugar_Crush.mp3"
       preload
@@ -12,10 +13,9 @@
 
 <script>
 export default {
-    name: 'SugarCrush',
-    props: ['visibility']
-
-}
+  name: "SugarCrush",
+  props: ["visibility"],
+};
 </script>
 
 <style>
@@ -33,7 +33,8 @@ export default {
 
 .sugar-crush h1 {
   color: #fff8dc;
-  text-shadow: -4px 0 firebrick, 0 4px firebrick, 4px 0 firebrick, 0 -4px firebrick;
+  text-shadow: -4px 0 firebrick, 0 4px firebrick, 4px 0 firebrick,
+    0 -4px firebrick;
   font-family: "Emilys Candy", cursive;
   font-size: 65px;
 }
