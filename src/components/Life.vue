fcb45eb288c3d41ed2b408f5446a50c9e1c6d91e
Attilius <leo20@freemail.hu>
2022-10-12 20:21:15 +0200

    Modified Life.vue

--- src/components/Life.vue
+++ src/components/Life.vue
@@ -8,7 +8,7 @@
 <script>
 export default {
   name: "Life",
-  
+
   mounted() {
     this.lifeRegeneration();
   },
@@ -20,35 +20,42 @@ export default {
 
     setLifeStatus() {
       return this.$store.getters.lifeStatus;
-    }
+    },
   },
 
   methods: {
     changeLifeInStore() {
-      this.$store.commit('increment', 1);
+      this.$store.commit("increment", 1);
     },
 
     changeLifeStatusInStore(value) {
-      this.$store.commit('changeLifeStatus', value);
+      this.$store.commit("changeLifeStatus", value);
     },
 
     lifeRegeneration() {
-        let min = 0;
-        let sec = 0;
-        let timecounter = "";
+      let min = 0;
+      let sec = 0;
+      let timecounter = "";
 
-      if (this.$store.getters.lifeStatus !== '' && this.$store.getters.lifeStatus !== "Max") {
-        min = parseInt(this.$store.getters.lifeStatus.split(':')[0].split('')[1]);
-        if (parseInt(this.$store.getters.lifeStatus.split(':')[1]) > 9) {
-          sec = parseInt(this.$store.getters.lifeStatus.split(':')[1]);
+      if (
+        this.$store.getters.lifeStatus !== "" &&
+        this.$store.getters.lifeStatus !== "Max"
+      ) {
+        min = parseInt(
+          this.$store.getters.lifeStatus.split(":")[0].split("")[1]
+        );
+        if (parseInt(this.$store.getters.lifeStatus.split(":")[1]) > 9) {
+          sec = parseInt(this.$store.getters.lifeStatus.split(":")[1]);
         } else {
-          sec = parseInt(this.$store.getters.lifeStatus.split(':')[1].split('')[1]);
+          sec = parseInt(
+            this.$store.getters.lifeStatus.split(":")[1].split("")[1]
+          );
         }
       } else {
         min = 9;
         sec = 60;
       }
-      
+
       const timer = setInterval(() => {
         if (sec > 0) {
           sec--;
@@ -75,50 +82,52 @@ export default {
         }
       }, 1000);
     },
-  }
+  },
 };
 </script>
 
 <style>
 .life {
-    display: flex;
-    justify-content: space-around;
-    align-items: center;
-    width: 20%;
-    height: 60%;
+  display: flex;
+  justify-content: space-around;
+  align-items: center;
+  width: 20%;
+  height: 60%;
 }
 
 #life-regeneration {
-    height: 100%;
-    width: 100%;
-    border-radius: 50px;
-    padding-left: 20px;
-    background-color: rgb(73, 62, 84);
-    border: 1px solid goldenrod;
-    font-family: 'Emilys Candy', cursive;
-    font-size: 22px;
-    text-shadow: -1px 0 firebrick, 0 1px firebrick, 1px 0 firebrick, 0 -1px firebrick;
-    display: flex;
-    justify-content: center;
-    align-items: center;
-    color: #FFF8DC;
+  height: 100%;
+  width: 100%;
+  border-radius: 50px;
+  padding-left: 20px;
+  background-color: rgb(73, 62, 84);
+  border: 1px solid goldenrod;
+  font-family: "Emilys Candy", cursive;
+  font-size: 22px;
+  text-shadow: -1px 0 firebrick, 0 1px firebrick, 1px 0 firebrick,
+    0 -1px firebrick;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  color: #fff8dc;
 }
 
 #heart {
-    height: 140%;
-    width: 55%;
-    background: url("../assets/images/heart.png") no-repeat center;
-    background-size: cover;
-    font-family: 'Emilys Candy', cursive;
-    font-size: 20px;
-    text-align: center;
-    text-shadow: -1px 0 firebrick, 0 1px firebrick, 1px 0 firebrick, 0 -1px firebrick;
-    display: flex;
-    justify-content: center;
-    align-items: center;
-    color: #FFF8DC;
-    position: relative;
-    bottom: 2px;
-    left: 35px;
+  height: 140%;
+  width: 55%;
+  background: url("../assets/images/heart.png") no-repeat center;
+  background-size: cover;
+  font-family: "Emilys Candy", cursive;
+  font-size: 20px;
+  text-align: center;
+  text-shadow: -1px 0 firebrick, 0 1px firebrick, 1px 0 firebrick,
+    0 -1px firebrick;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  color: #fff8dc;
+  position: relative;
+  bottom: 2px;
+  left: 35px;
 }
 </style>
