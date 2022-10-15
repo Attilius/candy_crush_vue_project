25715466e0080c3f02548a9dcfbd540b39ce5552
Attilius <leo20@freemail.hu>
2022-10-15 11:43:01 +0200

    Modified index.js (src/store)

--- src/store/index.js
+++ src/store/index.js
@@ -282,6 +282,10 @@ export default new Vuex.Store({
                 completed: false,
                 stars: 0
             },
+            "level_8": {
+                completed: false,
+                stars: 0
+            },
         },
         life: 20,
         life_status: '',
