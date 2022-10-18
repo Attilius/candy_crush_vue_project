186b10ec49f04138be82cf55887dd01af2211241
Attilius <leo20@freemail.hu>
2022-10-18 19:59:24 +0200

    Modified index.js (src/store)

--- src/store/index.js
+++ src/store/index.js
@@ -271,7 +271,7 @@ export default new Vuex.Store({
             },
             "level_4": {
                 completed: false,
-                best_score: 0,
+                highest_score: 0,
                 stars: 0
             },
             "level_5": {
