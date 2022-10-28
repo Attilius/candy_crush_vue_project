ebd235ce9e7627d7aece665b4d5971b11278a5ba
Attilius <leo20@freemail.hu>
2022-10-28 19:07:35 +0200

    Modified BonusBoard.vue

--- src/components/BonusBoard.vue
+++ src/components/BonusBoard.vue
@@ -2,7 +2,7 @@
   <div class="bonus-board">
     <img src="../assets/images/hand.png" id="hand" alt="hand">
     <img src="../assets/images/lollipop.png" id="lollipop" alt="lollipop">
-
+    <img >
     <SoundButton id="sound-btn" />
   </div>
 </template>
